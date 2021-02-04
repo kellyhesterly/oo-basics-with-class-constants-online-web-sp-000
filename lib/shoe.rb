66f877ec8bed889a4fecac83645ef481ddadd854
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-04 21:27:06 +0000

    Automatically backed up by Learn

---
+++ lib/shoe.rb
@@ -0,0 +1,18 @@
+class Shoe
+
+
+  attr_accessor :color, :size, :material, :condition, :brand
+
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def cobble
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+
+
+
+end
